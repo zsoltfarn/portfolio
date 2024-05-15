c88c08fd6708032834ba66cad9c680492a3ff74e
Zsolt Farnas <zsolt.farnas@gmail.com>
2024-05-15 18:51:04 +0200

    send mail update

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,14 @@ sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/*===== SEND MAIL =====*/
+function sendEmail() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var mailtoLink = "mailto:zsolt.farnas@gmail.com" + "?subject=Message from " + name + "&body=" + encodeURIComponent(message + "\n\nSender's email: " + email);
+
+    window.location.href = mailtoLink;
+}
